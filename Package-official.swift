d39cf1ea7771a52c572e738d4a80ec4943020f89
TimorYang <10718837+TimorYang@users.noreply.github.com>
2026-04-23 14:21:41 +0000

    Libbox Release v1.13.11

--- Package-official.swift
+++ Package-official.swift
@@ -11,8 +11,8 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "Libbox",
-      url: "https://github.com/TimorYang/sing-box-lib/releases/download/v1.13.10/Libbox-ios-official.xcframework.zip",
-      checksum: "9e989daf11e0afc5540ffb9871ec9e2102bd9e5fb7f250045ee5fa0af566e39c"
+      url: "https://github.com/TimorYang/sing-box-lib/releases/download/v1.13.11/Libbox-ios-official.xcframework.zip",
+      checksum: "c695991f7874cebcb80228209398e05e86130ec09f9ddabfe0c2ba5f462bafdd"
     )
   ]
 )
--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "Libbox",
-      url: "https://github.com/TimorYang/sing-box-lib/releases/download/v1.13.10/Libbox-ios.xcframework.zip",
-      checksum: "7803d0ec33895513298f7e0e91776bc26047a33d3b9e808d0c4abf0ccb294cb8"
+      url: "https://github.com/TimorYang/sing-box-lib/releases/download/v1.13.11/Libbox-ios.xcframework.zip",
+      checksum: "4d4048f54c89abd32b04ead7ec181fa8a1de825b6a97bd15a0750c137f5d22ec"
     )
   ]
 )
